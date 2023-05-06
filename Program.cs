86179e9541d7c5ab9969bb06c8002353bfc7541a
ChristopherBroendholt <c.broendholt@gmail.com>
2023-05-06 16:01:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Turtle;
+
+public static class Shell{
+
+    public static void Main(){
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+
+        string[] name = new string[]{"T", "u", "r", "t", "l", "e", "-", "s", "h", "e", "l", "l"};
+
+        
+        for(int i = 0; i < name.Length; i++){
+            if(i % 2 == 0){
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+            }
+            else{
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            }
+
+            Console.Write(name[i]);
+
+            Thread.Sleep(100);
+        }
+
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("\n🐢  The slowest shell available  🐢");
+
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
